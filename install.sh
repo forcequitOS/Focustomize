217494989b25edb203a3f4be9b50b68b044bd72d
forcequitOS <62081155+forcequitOS@users.noreply.github.com>
2024-11-27 00:02:36 -0500

    Last installer update hopefully

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Focustomize Installer
+# Focustomize Installer 1.0.5
 # Define the download location and the URL for the latest release
 DOWNLOAD_DIR=~/Downloads
 ZIP_FILE=Focustomize.zip
@@ -8,23 +8,24 @@ TEMP_DIR=$(mktemp -d)
 GITHUB_RELEASE_URL="https://github.com/forcequitOS/Focustomize/releases/latest/download/Focustomize.zip"
 
 # Just hides everything. Makes it look clean and cool.
-exec 3>&1 4>&2
-exec > >(grep -v -e "^" -e "^.*$") 2>&1
+function silence() {
+    "$@" > /dev/null 2>&1
+}
 
 # Checks if Focustomize has already been downloaded (this is a pretty lazy check)
 if [ ! -f "$DOWNLOAD_DIR/$ZIP_FILE" ]; then
     echo "Downloading Focustomize..."
     # Downloads latest Focustomize
-    curl -L -o "$DOWNLOAD_DIR/$ZIP_FILE" "$GITHUB_RELEASE_URL"
+    silence curl -L -o "$DOWNLOAD_DIR/$ZIP_FILE" "$GITHUB_RELEASE_URL"
     
     # Extracts zip to a temporary location
-    unzip -qq "$DOWNLOAD_DIR/$ZIP_FILE" -d "$TEMP_DIR"
+    silence unzip "$DOWNLOAD_DIR/$ZIP_FILE" -d "$TEMP_DIR"
     rm "$DOWNLOAD_DIR/$ZIP_FILE"
 else
-	# Skips download
+	# Skips download (Since there's already a copy of Focustomize.zip locally)
     echo "Using pre-existing Focustomize archive"
     # Extracts zip
-    unzip -qq "$DOWNLOAD_DIR/$ZIP_FILE" -d "$TEMP_DIR"
+    silence unzip -qq "$DOWNLOAD_DIR/$ZIP_FILE" -d "$TEMP_DIR"
 fi
 
 if [ -d "$TEMP_DIR/$APP_NAME" ]; then
@@ -40,12 +41,9 @@ if [ -d "$TEMP_DIR/$APP_NAME" ]; then
 
 	# Ad-hoc signs app
     echo "Signing Focustomize"
-    codesign -f -s - --deep /Applications/$APP_NAME
+    silence codesign -f -s - --deep /Applications/$APP_NAME
 	# Yippee.
     echo -e "\033[0;32mFocustomize has been installed successfully!\033[0m"
 else
     echo "Error: Could not find Focustomize.app in the extracted contents."
 fi
-
-# Unhides everything.
-exec 1>&3 2>&4
